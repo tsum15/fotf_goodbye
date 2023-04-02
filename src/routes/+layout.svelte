9bd9fb74e84cd508498e9a448c09d9b9959a8843
Tyler <tyler.somers15@gmail.com>
2023-04-01 21:24:50 -0500

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.postcss';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
