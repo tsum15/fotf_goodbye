7cb4b396194fa6bdb37cddf028e219b8ec5eb1a7
Tyler <tyler.somers15@gmail.com>
2023-04-01 21:04:35 -0500

    font size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,31 @@
-<section class="mx-auto max-w-[70%]">
-	<p class="text-lg font-medium text-gray-500 pb-2">
+<section class="mx-auto max-w-[90%] md:max-w-[70%]">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		“Should an emergency situation occur, you need to put your own oxygen mask on first before
 		attempting to help those around you.”
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		As much as we’ve heard this on every flight we’ve ever been on in the history of ever, putting
 		ourselves first can be really hard… selfish even. After 4.5 years of running Stay + Play Local,
 		helping our partners, our audience, our community, and our team before helping myself, I decided
 		to selfishly put my oxygen mask on first.
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		This isn’t goodbye because you’ll see me, the S+PL girls, and our families around town. This is
 		still our beloved Tri-Cities. However, this IS good-bye from S+PL.
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		Owning a business is hard, friends. Business owners prioritize their customers, the community,
 		fellow business owners, their team; making sure everyone in those groups is happy and satisfied…
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		…But that often comes at the owner’s (and their family’s) expense.
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		While this was certainly a dreamy job, I was still working when we were out and about. And in
 		the evenings, I sacrificed time with my family for networking events, nonprofit board positions,
 		and other work engagements.
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		Sometimes businesses close not for financial reasons, but simply because… owning a business is
 		hard. It is for that exact reason that I decided it was time for something new for me and my
 		fam. I have recently accepted a position as the Senior Educational Marketing Specialist for a
@@ -33,12 +33,12 @@
 		winning entrepreneur from Forbes Magazine, and I’m POSITIVE that I will succeed beyond my own
 		expectations in my new position. THANK YOU!!!!
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		You’ve all been SUCH an integral part to the success of Stay + Play Local and I am forever
 		grateful for the endless support you offered as a I navigated the world of small business
 		ownership with you all by my side
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">
 		🖤 If you’d like to follow me and see what mischief I’m up to, find me on Instagram
 		<a
 			class="text-cyan-600"
@@ -47,7 +47,7 @@
 		>. Until then, friends, sending hugs and endless gratitude straight from me to you. P.S. This is
 		what the beginning of one chapter and the start of the next looks like 🖤
 	</p>
-	<p class="text-lg font-medium text-gray-500 pb-2">-xoxo-Chrissy</p>
+	<p class="text-md md:text-lg font-medium text-gray-500 pb-2">-xoxo-Chrissy</p>
 </section>
 <section class="pb-8">
 	<img
